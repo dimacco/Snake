512f8b9165327f5ef9f0d82b679a5375d78d65b2
dimacco <dimacco@gmail.com>
2017-01-16 20:01:00 +0300

    Инкапсуляция

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,23 +10,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
-
+            Point p1 = new Point(1,3,'*');
             p1.Draw();
 
-            /*int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';*/
-
-
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
-
+            Point p2 = new Point(4,5,'#');
             p2.Draw();
 
             Console.ReadLine();
